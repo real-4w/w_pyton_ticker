1ba91b101c98156b6d6f79a494162f6366afc52f
Real-4W <real_4w@protonmail.com>
2023-06-04 10:20:28 +1200

    WIP

--- w_python_ticker_full.py
+++ w_python_ticker_full.py
@@ -9,8 +9,7 @@ def load_ticker_text(file_path):
             print(exc)
     return data['messages']
 
-# Ticker Tape Class
-class TickerTape:
+class TickerTape:                                                                                                               
     """
     TickerTape is a class that creates a scrolling ticker tape in a tkinter window.
 
@@ -36,51 +35,44 @@ class TickerTape:
             messages (list): The messages to be displayed.
         """
         self.master = master
-        self.messages = cycle(messages) # to cycle through the list of messages
+        self.messages = cycle(messages)                                                                                     # to cycle through the list of messages
         self.label = tk.Label(master, text='', font=("Helvetica", 16), bg='black', fg='white', anchor='w')
         self.label.pack(fill='x')
 
-    # Run the ticker
     def run(self):
         """
         Starts the ticker tape.
         """
-        self.master.after(1000, self.update) # Update every 1 second
+        self.master.after(1000, self.update)                                                                                # Update every 1 second
 
-    # Update the label with the next message
     def update(self):
         """
         Updates the label with the next message from the cycle.
         """
         message = next(self.messages)
-        message = message + ' ' * 200  # pad the message with spaces to fit screen width 
-        self.show_message(message, message)
+        message = ' ' * 300 + message                                                                                       # pad the message with spaces to fit screen width
+        self.show_message(message)
 
-
-    def show_message(self, full_message, current_message):
+    def show_message(self, message):
         """
         Displays the current message and recursively calls itself to scroll the message.
 
         Args:
-            full_message (str): The complete message that is being displayed.
-            current_message (str): The current state of the message as it scrolls.
+            message (str): The current state of the message as it scrolls.
         """
-        self.label.config(text=current_message)
-        if len(full_message) > 0:
-            # This will create a rotating effect
-            next_message = full_message[1:] + full_message[0]
-            self.master.after(100, lambda: self.show_message(next_message, next_message))
+        self.label.config(text=message)
+        if len(message) > 1:                                                                                                # This will create a scrolling effect
+            next_message = message[1:]
+            self.master.after(100, lambda: self.show_message(next_message))
         else:
-            self.master.after(1000, self.update)  # Update after 1 second when there's no more message to scroll
+            self.master.after(1000, self.update)                                                                            # Update after 1 second when there's no more message to scroll
 
-
-# Main
-if __name__ == "__main__":
+if __name__ == "__main__":                                                                                                  # Main body
     messages = load_ticker_text('ticker_text.yaml')
     root = tk.Tk()
-    screen_width = root.winfo_screenwidth()  # get the screen width
-    root.geometry(f"{screen_width}x60")  # set the window width to the screen width
-    root.title("My Ticker Tape")  # set the window title here
+    screen_width = root.winfo_screenwidth()                                                                                 # get the screen width
+    root.geometry(f"{screen_width}x60")                                                                                     # set the window width to the screen width
+    root.title("My Ticker Tape")                                                                                            # set the window title here
     ticker = TickerTape(root, messages)
     ticker.run()
     root.mainloop()
